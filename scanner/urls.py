2dc81d7e5d073a752daebcce5c2ed1000350759f
Jayaprakash <syamalajayaprakashreddy@gmail.com>
2026-05-20 01:29:16 +0100

    fix: remove duplicate google_auth import

--- scanner/urls.py
+++ scanner/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .webhook import stripe_webhook
-from .scan_views import scan_history, google_auth
+from .scan_views import scan_history
 
 urlpatterns = [
     # existing
