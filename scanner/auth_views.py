2ceb9ef227af726c552b0058ced4ab16a3d5ada5
Jayaprakash <syamalajayaprakashreddy@gmail.com>
2026-05-22 09:38:04 +0100

    fix: add api_view import

--- scanner/auth_views.py
+++ scanner/auth_views.py
@@ -3,6 +3,7 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view
 
 
 class RegisterView(APIView):
