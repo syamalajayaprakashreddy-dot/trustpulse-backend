32869cca3c9be672ca423f2371686eed42d9df62
Jayaprakash <syamalajayaprakashreddy@gmail.com>
2026-05-23 11:59:24 +0100

    fix: add JsonResponse to top imports

--- scanner/views.py
+++ scanner/views.py
@@ -6,7 +6,7 @@ import os
 import json
 import hashlib
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
